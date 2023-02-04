24001aa811f654c1c5ef2145df33e111cc15e769
Janne Kauppinen <janne.a.kauppinen@student.jyu.fi>
2023-02-04 19:12:20 +0200

    Works

--- src/cards.rs
+++ src/cards.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use std::borrow::Cow;
 use strong_xml::{XmlRead, XmlWrite};
 use serde::{Serialize, Deserialize};
@@ -8,6 +9,11 @@ use std::path::Path;
 use std::io::prelude::*;
 use std::error::Error;
 use std::collections::HashMap;
+
+pub struct Booster {
+    pub set: String,
+    pub amount: u32, 
+}
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Listofcarddatafiles {
@@ -21,11 +27,12 @@ pub struct Filetoinclude  {
     file: String
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct CardInput {
     name: String,
     set: String,
     imagefile: String,
+    rarity: String,
 }
 
 // LACKEY output formats.
@@ -67,7 +74,7 @@ pub struct SuperZone<'a> {
 
 #[derive(XmlWrite, XmlRead, PartialEq, Debug, Clone)]
 #[xml(tag = "card")]
-struct Card<'a> {
+pub struct Card<'a> {
     #[xml(child = "name")]
     name: Name<'a>,
     #[xml(child = "set")]
@@ -103,8 +110,81 @@ pub struct CardName {
     #[serde(rename = "$value")]
     name: String, 
 }
+ 
+pub fn buy_boosters<'a>(boosters: &'a Vec<Booster>, sets: &'a mut HashMap<String, Vec<CardInput>>) -> Vec<Card<'a>> {
 
-pub fn to_lackey_test() {
+    let mut result = Vec::<Card>::new();
+
+    for booster in boosters {
+        
+        let rare = String::from("R");
+        let unc = String::from("U");
+        let com = String::from("C");
+
+        let mut rares = Vec::<CardInput>::new(); 
+        let mut uncommons = Vec::<CardInput>::new(); 
+        let mut commons = Vec::<CardInput>::new(); 
+
+        // Filter set using rarity.
+        let the_set = sets.get(&booster.set).unwrap();
+
+        //             .unwrap()
+        //             .into_iter()
+        //             .filter(|x| x.rarity.eq(&rare))
+        //             .collect_into(&mut rares);
+        for i in the_set.iter() {
+            if i.rarity.eq(&rare) {
+               rares.push(i.clone());
+            }
+            else if i.rarity.eq(&unc) {
+               uncommons.push(i.clone());
+            }
+            else if i.rarity.eq(&com) {
+               commons.push(i.clone());
+            }
+        }
+
+        let rare_count = booster.amount; 
+        let uncommon_count = booster.amount * 3; 
+        let common_count = booster.amount * 11; 
+
+        println!("{:?}", rares.len()); 
+        println!("{:?}", uncommons.len()); 
+        println!("{:?}", commons.len()); 
+
+        let mut rng = thread_rng();
+
+        // Get Rare cards.
+        for i in 0..rare_count {
+            let ind = rng.gen_range(0..rares.len()) as usize; 
+            result.push(
+                Card { name: Name {id: rares[ind].imagefile.clone().into(),
+                                   name: rares[ind].name.clone().into()},
+                       set: Set { name: rares[ind].set.clone().into()},
+                });
+        } 
+        for i in 0..uncommon_count {
+            let ind = rng.gen_range(0..uncommons.len()) as usize; 
+            result.push(
+                Card { name: Name {id: uncommons[ind].imagefile.clone().into(),
+                                   name: uncommons[ind].name.clone().into()},
+                       set: Set { name: uncommons[ind].set.clone().into()},
+                });
+        } 
+        for i in 0..common_count {
+            let ind = rng.gen_range(0..commons.len()) as usize; 
+            result.push(
+                Card { name: Name {id: commons[ind].imagefile.clone().into(),
+                                   name: commons[ind].name.clone().into()},
+                       set: Set { name: commons[ind].set.clone().into()},
+                });
+        } 
+
+    }
+    result
+}
+
+pub fn to_lackey(cards: &Vec<Card>) -> String {
 //    let deck = Deck {
 //        version: "0.8".to_string(),
 //        meta: Meta { game: "magic".to_string()},
@@ -121,25 +201,25 @@ pub fn to_lackey_test() {
         meta: Meta { game: Game {name:std::borrow::Cow::Borrowed("magic")}}.into(),
         super_zone: SuperZone {
             name: std::borrow::Cow::Borrowed("Deck"),
-            cards: vec![
-                Card {name: Name {id: "id heh".into(),
-                                  name: "Fireballi".into()},
-                      set: Set { name: "4e".into()},
-                },
-                Card {name: Name {id: "id 123".into(),
-                                  name: "Timber woloffi".into()},
-                      set: Set { name: "3e".into()},
-                }
-            ],
+            cards: cards.clone(),
+            // cards: vec![
+            //     Card {name: Name {id: "id heh".into(),
+            //                       name: "Fireballi".into()},
+            //           set: Set { name: "4e".into()},
+            //     },
+            //     Card {name: Name {id: "id 123".into(),
+            //                       name: "Timber woloffi".into()},
+            //           set: Set { name: "3e".into()},
+            //     }
+            // ],
         }.into(),
 
         //meta: Meta { game: std::borrow::Cow::Borrowed("erkki")}.into(),
 
         //meta: Game { name: "mtg".into() }.into(),
     }; 
-
+    deck.to_string().unwrap()
     // let mut buffer = Vec::new();
-    println!("{:?}", deck.to_string());
 
 }
 
@@ -149,10 +229,6 @@ pub fn load_list_of_cards() -> Result<String, Box<dyn Error>>  {
     let mut buffer = String::new();
     f.read_to_string(&mut buffer)?;
     Ok(buffer)
-}
-
-pub fn to_lackey(cards: &Vec<CardInput>) {
-
 }
 
 // pub fn load_cards() -> Vec<Card> {
@@ -182,7 +258,7 @@ pub fn load_mtg() -> HashMap<String, Vec<CardInput>> {
                         let ll = ip.split("\t").collect::<Vec<&str>>();
                         if ll.len() < 2 { continue; }
 
-                        set_hash_map.entry(ll[1].to_string()).or_default().push(CardInput { name: ll[0].to_string(), set: ll[1].to_string(), imagefile: ll[2].to_string() });
+                        set_hash_map.entry(ll[1].to_string()).or_default().push(CardInput { name: ll[0].to_string(), set: ll[1].to_string(), imagefile: ll[2].to_string(), rarity: ll[12].to_string() });
                     }
         	}
         }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,32 @@
 use boosterSimulator::cards::*;
 use dioxus::prelude::*;
 use serde_xml_rs::{from_str, to_string};
+use core::mem::replace;
+use std::fs;
+
 
 fn main() {
 //    pub struct Listofcarddatafiles {
 //    pub struct Filetoinclude  {
     //let card = Point {x:1, y:2}; 
-    let card_lists: Listofcarddatafiles  = from_str(&load_list_of_cards().unwrap()).unwrap();
-   to_lackey_test();
+    // let card_lists: Listofcarddatafiles  = from_str(&load_list_of_cards().unwrap()).unwrap();
+    // to_lackey_test();
     //println!("{:?}", card_lists);
     //println!("{:?}", load_list_of_cards().unwrap());
-    load_mtg();
+    //load_mtg();
 
     //dioxus_desktop::launch(app);
+    let boosters = vec![
+	    Booster { set: "4e".to_string(), amount: 10, }, 
+    ];
+    let mut sets = load_mtg();
+    let cards =  buy_boosters(&boosters, &mut sets);
+    let lackey_filu = to_lackey(&cards);
+    fs::write("eikujoo.dek", lackey_filu).expect("Unable to write file");
+    //let lackey_filu_siisitty = lackey_filu.replace(&"\\\"", &"\"");
+    //println!("{:?}", lackey_filu_siisitty);
+    //println!("{:?}", r#"heko="just joo"#);
+    
 }
 
 fn app(cx: Scope) -> Element {
